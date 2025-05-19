57e83eb8f7c682bbbaebbc26d46f197b98d3e52d
sandeshnilas <sandeshnilaskhatwada@gmail.com>
2025-05-19 13:56:51 +0545

    this is for routes teaching

--- basic_js/index.js
+++ basic_js/index.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const app = express();
+const indexRoutes = require("./routes/indexRoutes");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use("/api", indexRoutes);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
